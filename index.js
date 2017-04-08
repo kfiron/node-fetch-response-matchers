ea589c975cc627ca41581f1c3e457f2e33b81b57
kfirb <kfirb@wix.com>
2017-04-08 18:40:59 +0300

    make main function nicer

--- index.js
+++ index.js
@@ -11,32 +11,47 @@ module.exports = (chai, utils) => {
       var result = {};
       var args = arguments;
       var that = this;
-      var derivedPromise = this._obj.then(res => {
-        result.res = res;
-        return result;
-      });
+      var derivedPromise = fetchResult(this._obj, result);
+      derivedPromise = enrichBodyIfBodyMatcher(derivedPromise,
+                                               result,
+                                               options.isBodyMatcher);
+      derivedPromise = assert(derivedPromise, options, that, args);
 
-      if(options.isBodyMatcher){
-        derivedPromise = derivedPromise.then(result => {
-          return result.res.text();
-        }).then(text => {
-          result.text = text;
-          return result;
-        });
-      }
-
-      derivedPromise = derivedPromise.then(result => {
-        this.assert(
-          options.predicate(result.res, result.text, args),
-          options.msgSuccess(args),
-          options.msgFail(args),
-          options.expected(args),
-          options.actual(result.res, result.text)
-        );
-      });
       // credit to chai-as-promised
       transferPromiseness(that, derivedPromise);
     });
+  }
+
+  function fetchResult(promise, result) {
+    return promise.then(res => {
+      result.res = res;
+      return result;
+    });
+  }
+
+  function enrichBodyIfBodyMatcher(promise, result, conditation) {
+    if (conditation) {
+      return promise.then(result => {
+        return result.res.text();
+      }).then(text => {
+        result.text = text;
+        return result;
+      });
+    } else {
+      return promise;
+    }
+  }
+
+  function assert(promise, options, that, args){
+    return promise.then(result => {
+      that.assert(
+        options.predicate(result.res, result.text, args),
+        options.msgSuccess(args),
+        options.msgFail(args),
+        options.expected(args),
+        options.actual(result.res, result.text)
+      );
+    })
   }
 
   function transferPromiseness(assertion, promise) {
